fa70892f53fa7f6408ef1abd1a7e930b025cf0d2
Emre Sefer <esefer@lanec1.compbio.cs.cmu.edu>
2015-08-26 13:45:00 -0400

    commit

--- goanalysis/readsumcount.py
+++ goanalysis/readsumcount.py
@@ -1,9 +1,10 @@
 import sys
 import os
 import math
+import operator
 
 def readMiRNAs():
-    """
+    """read mirnas
     """
     fname = "LCM-mir.txt"
     mirs = []
@@ -35,17 +36,17 @@ def readClusters():
     return clust2mir
 
 def getTops():
+    """return tops
     """
-    """
-    resfolder = "results"
-    outfolder = "sortedresults"
-    resdict = {}
-    smalllist = []
+    resfolder = "subsetontology10"
+    outfolder = "sorted{0}".format(resfolder)
     if not os.path.exists(outfolder):
        os.makedirs(outfolder) 
     for fname in os.listdir(resfolder):
         if fname.find("Store")!=-1:
-           continue 
+           continue
+        resdict = {}
+        smalllist = []
         fpath = "{0}/{1}".format(resfolder,fname)
         outfpath = "{0}/{1}".format(outfolder,fname)
         if os.path.exists(outfpath):
@@ -58,7 +59,7 @@ def getTops():
                       outfile.write(line+"\n")
                       continue
                    splitted = line.split("\t")
-                   pval = splitted[4]
+                   pval = splitted[3]
                    if pval.find("<")!=-1:
                       smalllist.append(line) 
                    else:
@@ -70,18 +71,17 @@ def getTops():
                 outfile.write(item+"\n")
             for keystr in sorted(resdict.keys()):
                 for item in resdict[keystr]:
-                    outfile.write(item+"\n")
-                    
+                    outfile.write(item+"\n")                
 
 getTops()
 exit(1)
                 
 datamirs = readMiRNAs()
-print len(datamirs)
 clust2mir = readClusters()
 
 mir2gene = {}
 count = 0
+mir2score = {}
 fname = "Summary_Counts.txt"
 with open(fname,"r") as infile:
     for line in infile:
@@ -90,35 +90,56 @@ with open(fname,"r") as infile:
            count += 1
            continue
         splitted = line.split("\t")
-        gene,mirname = splitted[1],splitted[-3]
+        gene,mirname,score = splitted[1],splitted[-3],splitted[-2]
+        score = float(score) if score!="NULL" else 0.0
         if splitted[3] != "10090":
            continue
         if mirname not in datamirs:
-           continue  
-        mir2gene.setdefault(mirname,set())
-        mir2gene[mirname].add(gene)
+           continue
+        mir2gene.setdefault(mirname,[])
+        if gene not in mir2gene[mirname]:
+           mir2gene[mirname].append(gene)
+           mir2score.setdefault(mirname,[])
+           mir2score[mirname].append(score)
 print len(mir2gene.keys())
+print mir2gene["mmu-miR-590-3p"]
 avggene = 0.0
 for geneim in mir2gene.keys():
     avggene += len(mir2gene[geneim])
 avggene /= float(len(mir2gene.keys()))
-print avggene    
+print avggene
+gene2mir = {}
+for tmir in mir2gene.keys():
+    for tgene in mir2gene[tmir]:
+        gene2mir.setdefault(tgene,[])
+        gene2mir[tgene].append(tmir)
+for tgene in gene2mir.keys():
+    assert len(set(gene2mir[tgene])) == len(gene2mir[tgene])
+print len(gene2mir.keys())                   
 
-genedir = "clustgenes"
+genedir = "subsetclustgenes"
 if not os.path.exists(genedir):
    os.makedirs(genedir)
 for clustid,curmirs in clust2mir.items():
     curgenes = []
+    curscores = []
     for curmir in curmirs:
         if not mir2gene.has_key(curmir):
            continue 
         curgenes.extend(mir2gene[curmir])
-    print clustid,len(curgenes)
+        curscores.extend(mir2score[curmir])
+    gene2score = {}
+    for tind,tgene in enumerate(curgenes):
+        gene2score[tgene] = curscores[tind]
+    sortedgenes = sorted(gene2score.items(), key=operator.itemgetter(1))
+    newcount = len(sortedgenes) / 10
+    usegenes = [tgene for tgene,tscore in sortedgenes[0:newcount]]
+    print clustid,len(usegenes)
     genepath = "{0}/{1}.txt".format(genedir,clustid)
     if os.path.exists(genepath):
        continue 
     with open(genepath,"w") as outfile:
-       for curgene in curgenes:
+       for curgene in usegenes:
            outfile.write("{0}\n".format(curgene))      
           
 
